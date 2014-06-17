97e6a64c70d922973bae1da84a8f026f4be99624
Ipsit Gaur <ipsit.gaur@optimusinfo.com>
2014-06-17 17:20:10 +0530

    SQL Basics Queries

--- SQL Induction/SQL Basics/SQL create table.sql
+++ SQL Induction/SQL Basics/SQL create table.sql
@@ -1,3 +1,3 @@
---creates a new table name employee with columns First name, Last name, Designation, Salary, Age and Half yearly grade with Designation 
+--creates a new table name employee with columns Employee Id, First name, Last name, Designation, Salary, Age and Half yearly grade with Designation 
 --having default value "Trainee" and Age having default value "18"
-create table employee (fname varchar(50),lname varchar(50),desig varchar(50) default 'Trainee',salary int,age int default 18,hyg int)
+create table employee (eid int NOT NULL, fname varchar(50),lname varchar(50),desig varchar(50) default 'Trainee',salary int,age int default 18,hyg int)
