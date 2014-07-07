6f033e710e96f41e085a2b67c73616f49f219267
Ipsit Gaur <ipsit.gaur@optimusinfo.com>
2014-07-07 17:48:49 +0530

    Dot Net Induction

---
+++ .NET Induction/Basics/Assignment 6/RailwayReservationSystem/RailwayReservationSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicketInformation[] tickets = new TicketInformation[5];
+            tickets[0] = new TicketInformation("Ipsit", 21, "Delhi", "Bareilly", "21/7/2014", 4233);
+            tickets[1] = new TicketInformation("Arun", 22, "Lucknow", "Bareilly", "22/7/2014", 1233);
+            tickets[2] = new TicketInformation("Shyam", 23, "Bhopal", "Delhi", "21/7/2014", 1111);
+            tickets[3] = new TicketInformation("Mohan", 24, "Chennai", "Mumbai", "22/7/2014", 2121);
+            tickets[4] = new TicketInformation("Gopal", 25, "Delhi", "Barabanki", "25/7/2014", 32323);
+
+
+            tickets[2].CancelTicket();
+            tickets[4].CancelTicket();
+            for (int counter = 0; counter < 5; counter++)
+            {
+                tickets[counter].DisplayInformation();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ .NET Induction/Basics/Assignment 6/RailwayReservationSystem/RailwayReservationSystem/TicketInformation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayReservationSystem
+{
+    class TicketInformation
+    {
+        #region private members
+        private string _passengerName;
+        private int _age;
+        private string _to;
+        private string _from;
+        private string _date;
+        private int _trainNumber;
+        private string _status;
+        #endregion
+
+        /// <summary>
+        /// Constructor for initializing a new TicketInformation's instance.
+        /// </summary>
+        /// <param name="passengerName">name of passenger</param>
+        /// <param name="age">age of passenger</param>
+        /// <param name="to">destination station</param>
+        /// <param name="from">initial station</param>
+        /// <param name="date">date of journey</param>
+        /// <param name="trainNumber">train number</param>
+        public TicketInformation(string passengerName, int age, string to, string from, string date, int trainNumber)
+        {
+            this._passengerName = passengerName;
+            this._age = age;
+            this._to = to;
+            this._from = from;
+            this._date = date;
+            this._trainNumber = trainNumber;
+            _status = "Booked";
+        }
+
+        /// <summary>
+        /// Displays information of the ticket.
+        /// </summary>
+        public void DisplayInformation()
+        {
+            Console.WriteLine("\n------Ticket Details----");
+            Console.WriteLine("Name of Passenger: {0}", _passengerName);
+            Console.WriteLine("Age of Passenger: {0}", _age);
+            Console.WriteLine("Boarding station: {0}", _to);
+            Console.WriteLine("Destination station: {0}", _from);
+            Console.WriteLine("Date of journey: {0}", _date);
+            Console.WriteLine("Train number: {0}", _trainNumber);
+            Console.WriteLine("Status of ticket: {0}", _status);
+        }
+
+        /// <summary>
+        /// Cancels the current ticket.
+        /// </summary>
+        public void CancelTicket()
+        {
+            _status = "Canceled";
+            Console.WriteLine("Ticket has been canceled.");
+        }
+    }
+}
